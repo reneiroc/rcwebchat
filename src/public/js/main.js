04a5ad1056237bfec38c8996e90eb1797c358a45
Reneiro <reneiroc@gmail.com>
2018-09-21 11:33:01 -0300

    Login bascico y mensajes funcionando

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,18 +5,34 @@
  
  const $message = $('#message');
  const $chat = $('#chat');
-//Dom 
  const $nickname = $('#nickName');
  const $nickError = $('#nickError');
  const $nickForm = $('#nickUserform');
 
-
- const $usersname = $('#usersName')
+ const $usersname = $('#usersNameList')
 
  $nickForm.submit(e =>{
      e.preventDefault();
-    socket.emit('new user', $nickname.val());
-     
+     //enviar usuario
+    socket.emit('new user', $nickname.val(), data =>{
+        if (data){
+            //MOstrar cuadro de mensajes y cuadro de Usuaiors
+            $('#chatWrap').show(); 
+            $('#userloginbox').hide()
+        }else{
+            //mostrar error
+            // Error no sale reivsar problema
+            $nickError.html(`
+                <div class = "alert alert-warning">
+                Usuario ya existe
+                </div>
+            `)
+        }    
+        $nickname.val('');
+        console.log(data);
+    } );
+    
+
  })
 
  $messageForm.submit( e => {
@@ -33,7 +49,15 @@
      $chat.append (e + '<br/>');
  })
 
- socket.on('new user', e => {
-    $usersname.append(e + '<br/>');
+ socket.on('newusers', data => {
+     let html= ''
+ 
+    for (let i=0; i < data.length; i++){
+
+        html += `<p> ${data[i]} </p>`
+    }
+    
+    $usersname.html(html);
+
 
  });
--- src/sockets.js
+++ src/sockets.js
@@ -1,7 +1,7 @@
 
-
+//Funcionalidad del Web Socket
 module.exports = function(io){
-
+    let nicknames = [];
      //Websocket escuchando
     io.on('connection', function(socket){
         console.log('a user connected');
@@ -11,9 +11,25 @@ module.exports = function(io){
             
         })
 
-        socket.on('new user', data =>{
-            io.sockets.emit('new user', data);
-            console.log(data);
+        socket.on('new user', (data, cb) =>{
+            
+            if (nicknames.indexOf(data) != -1 ) {
+                cb(false);
+                // console.log(false)
+                
+
+            }else {
+                cb(true);
+                // console.log(true);
+                socket.nickname = data;
+                nicknames.push(data);
+                //emitir vento para envair a los sockets los usuaiors
+                io.sockets.emit('newusers', nicknames);
+            }
+
+
+        
+
         })
 
     });
